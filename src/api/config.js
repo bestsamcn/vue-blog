25a5c432a93ea15ac04a100fba72f965fda52ba4
bestsamcn <645298225@qq.com>
2017-05-27 17:33:21 +0800

    :memo: 配置修改

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
-export const ROOT_API = 'http://blog.bestsamcn.me:81/api';
-export const POSTER_URL = 'http://blog.bestsamcn.me:81/public/img';
-export const FACE_URL = 'http://blog.bestsamcn.me:81/public/face';
+export const ROOT_API = 'http://api.bestsamcn.me/api';
+export const POSTER_URL = 'http://api.bestsamcn.me/public/img';
+export const FACE_URL = 'http://api.bestsamcn.me/public/face';
 // export const ROOT_API = 'http://localhost:3040/api';
 // export const POSTER_URL = 'http://localhost:3040/public/img';
